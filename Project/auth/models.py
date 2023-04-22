0d80d980c65815f3881f0a3f7d7ce54f623a5242
Soumilee <soumilee5110@gmail.com>
2023-04-21 22:59:53 -0400

    adding the work done today

--- Project/auth/models.py
+++ Project/auth/models.py
@@ -1,25 +1,40 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from base_model import db
-
-
 class User(UserMixin, db.Model):
     username = db.Column(db.String(50), index=True, unique=True)
     email = db.Column(db.String(150), unique=True, index=True)
     password_hash = db.Column(db.String(150))
     roles = db.relationship("UserRoles", back_populates="user")
-    account = db.relationship("Account", back_populates="user", uselist=False)
+    account = db.relationship("Account", back_populates="user")
 
+    def __init__(self, id, email, username, roles = []):
+        self.id = id
+        self.email = email
+        self.username = username
+        # pseudo-serializer for loading from json (map dict role to Role)
+        if roles and type(roles[0]) == dict:
+            from roles.models import Role
+            roles = [Role(**r) for r in roles]
+        self.roles = roles
+        self.authenticated = False
+    def is_active(self):
+        return self.is_active()
+    def is_anonymous(self):
+        return False
+    def is_authenticated(self):
+        return self.authenticated
+    def is_active(self):
+        return True
+    def get_id(self):
+        return str(self.id)
     def has_role(self, role):
         for assoc in self.roles:
             if assoc.role.name == role:
                 return True
         return False
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
