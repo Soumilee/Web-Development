30a1abef61fd2302206821f7cf3171d916cd1092
Soumilee <soumilee5110@gmail.com>
2023-04-22 21:58:30 -0400

    adding some files

--- Project/accounts/models.py
+++ Project/accounts/models.py
@@ -1,52 +1,44 @@
-from base_model import db
+from flask_login import UserMixin
+from common.utils import JsonSerializable
+from accounts.models import Account
 
-# https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#one-to-one
-from flask import flash
-from sqlalchemy import func
-from sqlalchemy.exc import SQLAlchemyError
-
-
-class Account(db.Model):
-    balance = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey("is601_user.id"))
-    user = db.relationship("User", back_populates="account")
-
-    def refresh(self):
-        b = db.session.query(func.sum(Transactions.diff)).filter(Transactions.src == self.id).scalar() or 0
-        self.balance = b
-        db.session.add(self)
-        try:
-            db.session.commit()
-            print("refreshed account")
-        except SQLAlchemyError as e:
-            print(e)
-
-
-class Transactions(db.Model):
-    src = db.Column(db.Integer, db.ForeignKey("is601_account.id"))
-    dest = db.Column(db.Integer, db.ForeignKey("is601_account.id"))
-    diff = db.Column(db.Integer)  # change in balance
-    reason = db.Column(db.String(15), index=True)  # transaction type (why it occurred)
-    details = db.Column(db.String(240))  # extra user readable details about the transaction
-
-    @staticmethod
-    def do_transfer(change, reason, src=-1, dest=-1, details=""):
-        # transactions will be two pairs (account losing the value and account gaining the value)
-        if change > 0:
-            t1 = Transactions(diff=(change * -1), src=src, dest=dest, reason=reason, details=details)
-            t2 = Transactions(diff=change, src=dest, dest=src, reason=reason, details=details)
-            db.session.add(t1)
-            db.session.add(t2)
-            try:
-                db.session.commit()
-                a1 = Account.query.get(src)
-                a2 = Account.query.get(dest)
-                a1.refresh()
-                a2.refresh()
+class User(UserMixin, JsonSerializable):
+    def __init__(self, id, email, username, account = Account(), roles = []):
+        self.id = id
+        self.email = email
+        self.username = username
+        if type(account) == dict:
+            account = Account(**account)
+        self.account = account
+        # pseudo-serializer for loading from json (map dict role to Role)
+        if roles and type(roles[0]) == dict:
+            from roles.models import Role
+            roles = [Role(**r) for r in roles]
+        self.roles = roles
+        self.authenticated = False
+    def get_account_id(self):
+        return self.account.id if self.account else -1
+    def get_balance(self):
+        return self.account.balance if self.account else 0
+    def is_active(self):
+        return self.is_active()
+    def is_anonymous(self):
+        return False
+    def is_authenticated(self):
+        return self.authenticated
+    def is_active(self):
+        return True
+    def get_id(self):
+        return str(self.id)
+    # usable in templates and views to check if user has a role
+    def has_role(self, role):
+        for r in self.roles:
+            if r.name in [role, "Admin"]: # if has Admin role, always allow
                 return True
-            except SQLAlchemyError as e:
-                print(e)
-                flash("Error creating transaction", "danger")
-        else:
-            flash("Invalid transaction amount", "warning")
+        return False
+    def has_one_of_roles(self, roles):
+        _roles = [list(roles), "Admin"]
+        for r in self.roles:
+            if r.name in _roles:
+                return True
         return False
