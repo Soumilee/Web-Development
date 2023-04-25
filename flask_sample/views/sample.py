97b4e47b746076f062790cae7d4de67d45d30a81
Soumilee <soumilee5110@gmail.com>
2023-04-25 14:59:25 -0400

    adding files for withdrawing and deposit into bank account

--- flask_sample/views/sample.py
+++ flask_sample/views/sample.py
@@ -31,6 +31,12 @@ def list_account():
     if account_number:
         query += " AND account_number == %s"
         args.append(f"%{account_number}%")
+    if account_type:
+        query += " AND account_type LIKE %s"
+        args.append(f"%{account_type}%")
+    if created:
+        query += " AND created == %s"
+        args.append(f"%{created}%")
     if limit and int(limit) > 0 and int(limit) <= 5:
         query += " LIMIT %s"
         args.append(int(limit))
@@ -45,13 +51,13 @@ def list_account():
         flash(e, "danger")    
     return render_template("list_account.html", resp=rows)
 
-@sample.route('/transaction', methods=['GET','POST']) # will perform deposit and withdraw 
+@sample.route('/transaction', methods=['GET','POST']) # will perform transaction in between accounts
 def commit_transaction():
     source_account = request.form.get("name")
     destination_account = request.form.get("col")
     amount = request.form.get("order")
     transaction_type = request.form.get("limit", 5)
-    memo = request.form.get()
+    memo = request.form.get("memo")
     if source_account is None:
         flash("The Source Bank Account must be inserted","error")
         return redirect("sample.commit_transaction")
@@ -74,3 +80,54 @@ def commit_transaction():
             except Exception as e:
                 flash(e, "danger")    
     return render_template("transactions.html")
+
+@sample.route('/deposit', methods=['GET','POST']) #this will perform deposit in which user will deposit money into their account from world account
+def deposit():
+    acc_no = request.form.get("acc_no")
+    type = request.form.get("type")
+    amt = request.form.get("amt")
+    if acc_no is None:
+        flash("Please select the account for deposit","error")
+    if type is None:
+        flash("Please select the type of account","error")
+    if amt<=1:
+        flash('Please select an amount greater than 1$',"error")
+    else:
+        if request.method == "POST":
+            try:                
+                balance = "SELECT balance FROM IS601_Accounts WHERE 1=1"
+                balance = + amt
+                world_bal = ("SELECT balance FROM IS601_Accounts WHERE account_umber = %s","000000000000")
+                result = DB.update("""UPDATE IS601_Accounts SET balance = %s WHERE account_number = %s """,balance,acc_no)
+                query = DB.insertOne("""INSERT INTO IS601_Transactions VALUES (account_src,account_dest,balance_change,transaction_type,memo) 
+                    VALUES(%s, %s, %s,%s,%s)""","000000000000",acc_no,amt,"deposit","deposit done by self")
+                world_bal = world_bal - amt
+                world_acc = DB.update("UPDATE IS601_Accounts SET balance = %s WHERE account_number = %s ",world_bal,"000000000000")
+                if result.status and query and world_acc:
+                    flash("The deposit has been submitted","message")
+            except Exception as e:
+                flash(e,"danger")
+    return render_template("deposit_into_acc.html")
+@sample.route('/withdraw', methods=['GET','POST']) #this will perform withdraw in which user will withdraw money from their account
+def withdraw():
+    acc_no = request.form.get("acc_no")
+    type = request.form.get("type")
+    amt = request.form.get("amt")
+    balance = "SELECT balance FROM IS601_Accounts WHERE 1=1"
+    if acc_no is None:
+        flash("Please select the account for deposit","error")
+    if type is None:
+        flash("Please select the type of account","error")
+    if amt<=1 or amt > balance:
+        flash('Please select an amount greater than 1$ and less than your total balance',"error")
+    else:
+        if request.method == "POST":
+            try:
+                balance = -amt
+                result = DB.update("UPDATE IS601_Accounts SET balance = %s WHERE account_number = %s ",balance,acc_no)
+                if result.status:
+                    flash("The money has been withdrawn","message")
+            except Exception as e:
+                flash(e,"danger")
+    return render_template("withdraw_from_acc.html")
+
